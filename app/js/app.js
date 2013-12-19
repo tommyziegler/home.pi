52365675bf0ff8440ff47015166b944d5e1ffbc0
Tommy Ziegler <privat@tommyz.de>
2013-12-19 18:38:31 +0100

    Update app.js

--- app/js/app.js
+++ app/js/app.js
@@ -4,7 +4,19 @@
 // Declare app level module which depends on filters, and services
 angular.module('homePiApp', ['homePiApp.filters', 'homePiApp.services', 'homePiApp.directives']).
   config(['$routeProvider', function($routeProvider) {
+    // Main App
     $routeProvider.when('/', {templateUrl: 'partials/switches.html', controller: SwitchListCtrl});
+    
+    // Contact
     $routeProvider.when('/contact', {templateUrl: 'partials/contact.html'});
+    
+    // Configuration
+      //$routeProvider.when('/configuration', {templateUrl: 'partials/configuration.html', controller: SwitchListCtrl});
+      //$routeProvider.when('/configuration/edit/:id', {templateUrl: 'partials/edit.html', controller: SwitchListCtrl});
+
+    // Contact
+    $routeProvider.when('/contact', {templateUrl: 'partials/contact.html'});
+
+    // All others
     $routeProvider.otherwise({redirectTo: '/'});
   }]);
